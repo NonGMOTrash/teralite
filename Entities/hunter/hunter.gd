dc7b8c56a6b7dff799d57917671c91b87f0d2970
NonGMOTrash <nongmotrash@gmail.com>
2021-11-24 21:13:22 -0800

    hunter awp reticle + better deagle sound

--- Entities/hunter/hunter.gd
+++ Entities/hunter/hunter.gd
@@ -13,16 +13,19 @@ onready var brain := $brain
 onready var held_item := $held_item
 onready var stats := $stats
 onready var animation := $AnimationPlayer
+onready var reticle := $reticle
 
 func _ready() -> void:
 	held_item.animation.connect("animation_finished", self, "attack")
 	actionable = true
+	reticle.visible = false
 
 func _on_action_lobe_action(action, target) -> void:
 	if actionable == false:
 		return
 	
 	if bullets == 0:
+		reticle.visible = false
 		queued_action = "reload"
 		held_item.animation.clear_queue()
 		held_item.animation.play("load", -1, 0.6)
@@ -32,12 +35,15 @@ func _on_action_lobe_action(action, target) -> void:
 	queued_action = "shoot"
 	targit = target
 	targit_path = target.get_path()
+	reticle.visible = true
 	for _i in warnings:
 		held_item.animation.queue("warn")
 
 func attack(finished_animation:String):
 	if held_item.animation.get_queue().size() > 0:
 		return
+	
+	reticle.visible = false
 	
 	if queued_action == "shoot":
 		if get_node_or_null(targit_path) == null:
@@ -58,6 +64,9 @@ func _physics_process(delta: float) -> void:
 		animation.play("stand")
 	else:
 		animation.play("walk")
+	
+	if reticle.visible and get_node_or_null(targit_path) != null and not targit == self:
+		reticle.global_position = targit.global_position
 
 func _on_brain_lost_target() -> void:
 	if brain.targets.size() == 0 and bullets != 10:
@@ -65,4 +74,5 @@ func _on_brain_lost_target() -> void:
 		held_item.animation.clear_queue()
 		held_item.animation.play("load", -1, 0.6)
 		actionable = false
+		reticle.visible = false
 	
